7883a14a1c3b0b8c6e76019052faf536627f7d25
Tim Westerhuis <tim.westerhuis@hotmail.com>
2025-03-23 17:36:10 +0100

    Continuing on Day6Part1
    Still stuck on Day2Part2 and Day3Part2...

--- src/main/java/day6/Day6Part1.java
+++ src/main/java/day6/Day6Part1.java
@@ -2,33 +2,51 @@ package day6;
 
 import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.HashSet;
+import java.util.HashMap;
 import java.util.Scanner;
 
 import static util.InputHandler.handleInputStream;
 
 public class Day6Part1 {
+
+    static ArrayList<String> inputArray = new ArrayList<>();
+
     public static void main(String[] args) {
 
         InputStream inputStream = handleInputStream("day6input.txt");
-        ArrayList<String> inputArray = new ArrayList<>();
         Scanner scanner = new Scanner(inputStream);
         while (scanner.hasNextLine()) inputArray.add(scanner.nextLine());
 
-        HashSet<Double> positionCoordinates = new HashSet<>();
+        HashMap<Integer, Integer> positionCoordinates = new HashMap<>();
+        int xAxis = 0, yAxis = 0;
 
         // Find the starting position (line and index)
-        for (int y = 0; y < inputArray.size(); y++) {
-            if (inputArray.get(y).indexOf('^') != -1) {
-                System.out.println("Starting position is: y-axis '" + y + "', x-axis '" + inputArray.get(y).indexOf('^') + "'");
-                positionCoordinates.add(y + inputArray.get(y).indexOf('^') / 1000.0);
+        for (; yAxis < inputArray.size(); yAxis++) {
+            if (inputArray.get(yAxis).indexOf('^') != -1) {
+                xAxis = inputArray.get(yAxis).indexOf('^');
+                positionCoordinates.put(yAxis, xAxis);
+                System.out.println("Starting position is: y-axis '" + yAxis + "', x-axis '" + xAxis + "'");
             }
         }
 
-        System.out.println(positionCoordinates);
 
 
 
+    }
+
+    static void moveUp(int yAxis, int xAxis) {
+
+    }
+
+    static void moveRight(int yAxis, int xAxis) {
+
+    }
+
+    static void moveDown(int yAxis, int xAxis) {
+
+    }
+
+    static void moveLeft(int yAxis, int xAxis) {
 
     }
 }
