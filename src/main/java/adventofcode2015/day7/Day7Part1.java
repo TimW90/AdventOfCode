f5024ad6db479ada74844af85352fb9fc68ee064
Tim Westerhuis <tim.westerhuis@hotmail.com>
2025-04-17 23:26:42 +0200

    Still working on Day7Part1 (2015)

--- src/main/java/adventofcode2015/day7/Day7Part1.java
+++ src/main/java/adventofcode2015/day7/Day7Part1.java
@@ -1,5 +1,6 @@
 package adventofcode2015.day7;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
@@ -9,6 +10,9 @@ import static util.InputHandler.handleInputStream;
 // Todo: Work in progress
 
 public class Day7Part1 {
+
+    static Map<String, String> test = new HashMap<>();
+
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(handleInputStream("2015/day7input.txt"));
@@ -21,8 +25,51 @@ public class Day7Part1 {
             instructions.put(wire, expression);
         }
 
-        instructions.forEach((key, value) -> System.out.println(key + " -> " + value));
+        //instructions.forEach((key, value) -> System.out.println(key + " -> " + value));
 
 
+        // test
+        test.put("ab", "a + b");
+        test.put("a", "5");
+        test.put("b", "c");
+        test.put("c", "10");
+
+        System.out.println("Value of ab = " + testMethod("ab"));
+    }
+
+    public static int testMethod(String key) {
+
+        int value = 0;
+
+        // ab = a + b
+        // a = 5, b = c;
+        // c = 10;
+
+        try {
+            String[] expressionSplit = test.get(key).split(" ");
+            // if expressionSplit.size() = 1 then parse the integer
+            // try testMethod(expressionSplit[0]) and try testMethod(expressionSplit[2]) ?
+            if (Arrays.asList(expressionSplit).contains("+")) {
+                value = (Integer.parseInt(expressionSplit[0]) + Integer.parseInt(expressionSplit[2]));
+            } else {
+                value = Integer.parseInt(test.get(key));
+            }
+
+        } catch (NumberFormatException e) {
+
+        }
+        return value;
+
+
+
+
+//        try {
+//            value = Integer.parseInt(test.get(key));
+//            System.out.println(key + " = " + value);
+//        } catch (NumberFormatException e) {
+//            value = testMethod(test.get(key));
+//            System.out.println(key + " = " + value);
+//        }
+//        return value;
     }
 }
