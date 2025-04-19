8fd600e8edcbf1852df93b8b30dadaea715ef178
Tim Westerhuis <tim.westerhuis@hotmail.com>
2025-04-19 21:18:25 +0200

    Day7Part1 has had some refactoring, but more is to come! Especially the 'evaluate()' method has to be smaller / cut into smaller methods

--- src/main/java/adventofcode2015/day7/Day7Part1.java
+++ src/main/java/adventofcode2015/day7/Day7Part1.java
@@ -19,14 +19,14 @@ public class Day7Part1 {
 
         while (scanner.hasNextLine()) {
             String[] lineSplit = scanner.nextLine().split(" -> ");
-            String wire = lineSplit[1];
+            String key = lineSplit[1];
             String expression = lineSplit[0];
 
             try {
                 int value = Integer.parseInt(expression);
-                instructions.put(wire, new Wire(value));
+                instructions.put(key, new Wire(value));
             } catch (NumberFormatException e) {
-                instructions.put(wire, new Wire(expression));
+                instructions.put(key, new Wire(expression));
             }
 
             //This is for part 2!
@@ -39,85 +39,82 @@ public class Day7Part1 {
 
 class Wire {
 
-   private Integer intValue;
+    private Integer intValue;
 
-   private final String expression;
+    private final String expression;
 
 
-   public Wire (Integer intValue) {
-       this.intValue = intValue;
-       this.expression = null;
-   }
+    public Wire(Integer intValue) {
+        this.intValue = intValue;
+        this.expression = null;
+    }
 
-   public Wire (String expression) {
-       this.intValue = null;
-       this.expression = expression;
-   }
+    public Wire(String expression) {
+        this.expression = expression;
+    }
 
-   private boolean hasInt() {
-       return intValue != null;
-   }
+    private boolean hasInt() {
+        return intValue != null;
+    }
 
-   public Integer evaluate(Map<String, Wire> instructions) {
-       if (hasInt()) return intValue;
+    public Integer evaluate(Map<String, Wire> instructions) {
+        if (hasInt()) return intValue;
 
-       String[] expressionSplit = expression.split(" ");
+        String[] expressionSplit = expression.split(" ");
 
-       // The wire is wire case
-       if (expressionSplit.length == 1) {
-           String key = expressionSplit[0];
-           Wire wire = instructions.get(key);
-           return intValue = wire.evaluate(instructions);
-       }
+        // The wire is wire case
+        if (expressionSplit.length == 1) {
+            String key = expressionSplit[0];
+            Wire wire = instructions.get(key);
+            return intValue = wire.evaluate(instructions);
+        }
 
-       // The NOT case
-       if (expressionSplit.length == 2) {
-           String key = expressionSplit[1];
-           Wire wire = instructions.get(key);
-           int value = wire.evaluate(instructions);
-           return intValue = ~value;
+        // The NOT case
+        if (expressionSplit.length == 2) {
+            String key = expressionSplit[1];
+            Wire wire = instructions.get(key);
+            int value = wire.evaluate(instructions);
+            return intValue = ~value;
 
-           // The other cases
-       } else if (expressionSplit.length == 3) {
-           String leftKey = expressionSplit[0];
-           String operator = expressionSplit[1];
-           String rightKey = expressionSplit[2];
+            // The other cases
+        } else if (expressionSplit.length == 3) {
+            String leftKey = expressionSplit[0];
+            String operator = expressionSplit[1];
+            String rightKey = expressionSplit[2];
 
-           int leftValue;
-           if (stringIsANumber(leftKey)) {
-               leftValue = Integer.parseInt((leftKey));
-           } else {
-               Wire leftWire = instructions.get(leftKey);
-               leftValue = leftWire.evaluate(instructions);
-           }
+            int leftValue;
+            if (stringIsANumber(leftKey)) {
+                leftValue = Integer.parseInt((leftKey));
+            } else {
+                Wire leftWire = instructions.get(leftKey);
+                leftValue = leftWire.evaluate(instructions);
+            }
 
-           int rightValue;
-           if (stringIsANumber(rightKey)) {
-               rightValue = Integer.parseInt(rightKey);
-           } else {
-               Wire rightWire = instructions.get(rightKey);
-               rightValue = rightWire.evaluate(instructions);
-           }
+            int rightValue;
+            if (stringIsANumber(rightKey)) {
+                rightValue = Integer.parseInt(rightKey);
+            } else {
+                Wire rightWire = instructions.get(rightKey);
+                rightValue = rightWire.evaluate(instructions);
+            }
 
-               intValue = switch (operator) {
-               case "AND" -> leftValue & rightValue;
-               case "OR" -> leftValue | rightValue;
-               case "RSHIFT" -> leftValue >> rightValue;
-               case "LSHIFT" -> leftValue << rightValue;
-               default -> throw new IllegalStateException("Unexpected value: " + operator);
-           };
+            return intValue = switch (operator) {
+                case "AND" -> leftValue & rightValue;
+                case "OR" -> leftValue | rightValue;
+                case "RSHIFT" -> leftValue >> rightValue;
+                case "LSHIFT" -> leftValue << rightValue;
+                default -> throw new IllegalStateException("Unexpected operator value: " + operator);
+            };
 
-           return intValue;
+        } else throw new IllegalArgumentException("Incorrect expression: " + expression);
+    }
 
-       } else throw new IllegalArgumentException("Incorrect expression: " + expression);
-   }
-
-   public boolean stringIsANumber(String str) {
-       try {
-           Integer.parseInt(str);
-           return true;
-       } catch (NumberFormatException e) {
-           return false;
-       }
-   }
+    public boolean stringIsANumber(String str) {
+        try {
+            Integer.parseInt(str);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
 }
