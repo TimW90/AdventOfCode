0fa3a593f89150aa3f014c82341c90aa8893b8a3
Tim Westerhuis <tim.westerhuis@hotmail.com>
2025-04-20 16:41:13 +0200

    Refactored Day2Part1 and Part2

--- src/main/java/adventofcode2015/day2/Day2Part1.java
+++ src/main/java/adventofcode2015/day2/Day2Part1.java
@@ -1,28 +1,28 @@
 package adventofcode2015.day2;
 
-import java.util.Scanner;
+import java.util.Arrays;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import static util.InputHandler.handleInputStream;
+import static util.FileReader.fileInResourcesToArray;
 
 public class Day2Part1 {
     public static void main(String[] args) {
-
-        Scanner scanner = new Scanner(handleInputStream("2015/day2input.txt"));
+        List<String> fileLines = fileInResourcesToArray("2015/day2input.txt");
         Pattern pattern = Pattern.compile("(\\d*)x(\\d*)x(\\d*)");
         int totalFeetOfWrappingPaper = 0;
 
-        while (scanner.hasNextLine()) {
-            String line = scanner.nextLine();
+        for (String line : fileLines) {
             Matcher matcher = pattern.matcher(line);
 
-            while (matcher.find()) {
-                int lengthWidth = Integer.parseInt(matcher.group(1)) * Integer.parseInt(matcher.group(2));
-                int widthHeight = Integer.parseInt(matcher.group(2)) * Integer.parseInt(matcher.group(3));
-                int heightLength = Integer.parseInt(matcher.group(3)) * Integer.parseInt(matcher.group(1));
-                totalFeetOfWrappingPaper += ((2 * lengthWidth) + (2 * widthHeight) + (2 * heightLength)
-                        + Math.min(lengthWidth, Math.min(widthHeight, heightLength)));
+            if (matcher.find()) {
+                int length = Integer.parseInt(matcher.group(1));
+                int width = Integer.parseInt(matcher.group(2));
+                int height = Integer.parseInt(matcher.group(3));
+                int[] sides = {length * width, width * height, height * length};
+                Arrays.sort(sides);
+                totalFeetOfWrappingPaper += 2 * sides[0] + 2 * sides[1] + 2 * sides[2] + sides[0];
             }
         }
         System.out.println("Total feet of wrapping paper: " + totalFeetOfWrappingPaper);
--- src/main/java/adventofcode2015/day2/Day2Part2.java
+++ src/main/java/adventofcode2015/day2/Day2Part2.java
@@ -1,30 +1,27 @@
 package adventofcode2015.day2;
 
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.IntStream;
 
-import static util.InputHandler.handleInputStream;
+import static util.FileReader.fileInResourcesToArray;
 
 public class Day2Part2 {
     public static void main(String[] args) {
-
-        Scanner scanner = new Scanner(handleInputStream("2015/day2input.txt"));
+        List<String> fileLines = fileInResourcesToArray("2015/day2input.txt");
         Pattern pattern = Pattern.compile("(\\d*)x(\\d*)x(\\d*)");
         int totalFeetOfRibbon = 0;
 
-        while (scanner.hasNextLine()) {
-            String line = scanner.nextLine();
+        for (String line : fileLines) {
             Matcher matcher = pattern.matcher(line);
 
-            while (matcher.find()) {
-                int[] numbers = {Integer.parseInt(matcher.group(1)),
-                        Integer.parseInt(matcher.group(2)),
-                        Integer.parseInt(matcher.group(3))};
-                Arrays.sort(numbers);
-                totalFeetOfRibbon += numbers[0] + numbers[0] + numbers[1] + numbers[1] +
-                        (numbers[0] * numbers[1] * numbers[2]);
+            if (matcher.find()) {
+                int[] numbers = IntStream.range(1, 4)
+                        .map(i -> Integer.parseInt(matcher.group(i)))
+                        .sorted()
+                        .toArray();
+                totalFeetOfRibbon += 2 * (numbers[0] + numbers[1]) + numbers[0] * numbers[1] * numbers[2];
             }
         }
         System.out.println("Total feet of ribbon: " + totalFeetOfRibbon);
