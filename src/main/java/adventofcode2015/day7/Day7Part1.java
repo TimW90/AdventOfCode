abad0d297f3fc44834c0fe35c230b00d1d0db234
Tim Westerhuis <tim.westerhuis@hotmail.com>
2025-04-22 18:49:48 +0200

    Refactored Day7Part1 though I am unsure about the method names 'evaluateExpression()', 'stringIsANumber()' and 'Wire' class
    But at least it looks better now (less code, smaller methods)

--- src/main/java/adventofcode2015/day7/Day7Part1.java
+++ src/main/java/adventofcode2015/day7/Day7Part1.java
@@ -1,10 +1,10 @@
 package adventofcode2015.day7;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-import java.util.Scanner;
 
-import static util.InputHandler.handleInputStream;
+import static util.FileReader.fileInResourcesToArray;
 
 // Todo: Work in progress
 
@@ -13,12 +13,11 @@ public class Day7Part1 {
     static Map<String, String> test = new HashMap<>();
 
     public static void main(String[] args) {
-
-        Scanner scanner = new Scanner(handleInputStream("2015/day7input.txt"));
+        List<String> fileLines = fileInResourcesToArray("2015/day7input.txt");
         Map<String, Wire> instructions = new HashMap<>();
 
-        while (scanner.hasNextLine()) {
-            String[] lineSplit = scanner.nextLine().split(" -> ");
+        for (String line : fileLines) {
+            String[] lineSplit = line.split(" -> ");
             String key = lineSplit[1];
             String expression = lineSplit[0];
 
@@ -28,10 +27,6 @@ public class Day7Part1 {
             } catch (NumberFormatException e) {
                 instructions.put(key, new Wire(expression));
             }
-
-            //This is for part 2!
-            // Overriding "b"
-            //instructions.put("b", new Wire(956));
         }
         System.out.println("The signal of wire 'a' is: " + instructions.get("a").evaluate(instructions));
     }
@@ -42,7 +37,6 @@ class Wire {
     private Integer intValue;
 
     private final String expression;
-
 
     public Wire(Integer intValue) {
         this.intValue = intValue;
@@ -61,55 +55,46 @@ class Wire {
         if (hasInt()) return intValue;
 
         String[] expressionSplit = expression.split(" ");
-
-        // The wire is wire case
-        if (expressionSplit.length == 1) {
-            String key = expressionSplit[0];
-            Wire wire = instructions.get(key);
-            return intValue = wire.evaluate(instructions);
-        }
-
+        // The assign (->) case
+        if (expressionSplit.length == 1) return intValue = getValue(expressionSplit[0], instructions);
         // The NOT case
-        if (expressionSplit.length == 2) {
-            String key = expressionSplit[1];
-            Wire wire = instructions.get(key);
-            int value = wire.evaluate(instructions);
-            return intValue = ~value;
+        if (expressionSplit.length == 2) return intValue = ~getValue(expressionSplit[1], instructions);
+        // The other cases
+        if (expressionSplit.length == 3) return intValue = evaluateExpression(expressionSplit, instructions);
 
-            // The other cases
-        } else if (expressionSplit.length == 3) {
-            String leftKey = expressionSplit[0];
-            String operator = expressionSplit[1];
-            String rightKey = expressionSplit[2];
-
-            int leftValue;
-            if (stringIsANumber(leftKey)) {
-                leftValue = Integer.parseInt((leftKey));
-            } else {
-                Wire leftWire = instructions.get(leftKey);
-                leftValue = leftWire.evaluate(instructions);
-            }
-
-            int rightValue;
-            if (stringIsANumber(rightKey)) {
-                rightValue = Integer.parseInt(rightKey);
-            } else {
-                Wire rightWire = instructions.get(rightKey);
-                rightValue = rightWire.evaluate(instructions);
-            }
-
-            return intValue = switch (operator) {
-                case "AND" -> leftValue & rightValue;
-                case "OR" -> leftValue | rightValue;
-                case "RSHIFT" -> leftValue >> rightValue;
-                case "LSHIFT" -> leftValue << rightValue;
-                default -> throw new IllegalStateException("Unexpected operator value: " + operator);
-            };
-
-        } else throw new IllegalArgumentException("Incorrect expression: " + expression);
+        throw new IllegalArgumentException("Incorrect expression: " + expression);
     }
 
-    public boolean stringIsANumber(String str) {
+    private int getValue(String key, Map<String, Wire> instructions) {
+        int value;
+
+        if (stringIsANumber(key)) {
+            value = Integer.parseInt((key));
+        } else {
+            Wire wire = instructions.get(key);
+            value = wire.evaluate(instructions);
+        }
+        return value;
+    }
+
+    private int evaluateExpression(String[] expressionSplit, Map<String, Wire> instructions) {
+        String leftKey = expressionSplit[0];
+        String operator = expressionSplit[1];
+        String rightKey = expressionSplit[2];
+
+        int leftValue = getValue(leftKey, instructions);
+        int rightValue = getValue(rightKey, instructions);
+
+        return switch (operator) {
+            case "AND" -> leftValue & rightValue;
+            case "OR" -> leftValue | rightValue;
+            case "RSHIFT" -> leftValue >> rightValue;
+            case "LSHIFT" -> leftValue << rightValue;
+            default -> throw new IllegalStateException("Unexpected operator value: " + operator);
+        };
+    }
+
+    private boolean stringIsANumber(String str) {
         try {
             Integer.parseInt(str);
             return true;
